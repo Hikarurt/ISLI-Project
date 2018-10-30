90b1a06bba46deec7045eb00f163ec7cb3601f26
Hikarurt <Hikarurt@outlook.com>
2018-10-30 15:40:33 +0800

    s
    a

---
+++ WindowsFormsAssembly/Assembly.DAL/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembly.DAL
+{
+    public class Product
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public int GetProductIDMax(int i)
+        {
+            return i * 10;
+        }
+
+        public decimal Price = 123.456m;
+
+        public int ID { get; set; }
+    }
+}
